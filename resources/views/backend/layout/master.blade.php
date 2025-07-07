98084c8b6bafed027f1f5a6fcf71de567d893d7c
tiaraelsha8 <elshawinatiara@gmail.com>
2025-07-07 11:31:31 +0700

    Update master.blade.php

--- resources/views/backend/layout/master.blade.php
+++ resources/views/backend/layout/master.blade.php
@@ -152,9 +152,9 @@
     <script src="{{ asset('templateadmin/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
     <script src="{{ asset('templateadmin/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
+    <script src="{{ asset('templateadmin/dist/js/adminlte.min.js') }}"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js"></script>
+    <script src="{{ asset('templateadmin/dist/js/demo.js') }}"></script>
     <!-- Page specific script -->
     <script>
         $(function() {
