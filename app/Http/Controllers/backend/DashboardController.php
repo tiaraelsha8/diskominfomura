1aee7604289611b32f64ed7617e8062932cbb698
unknown <elshawinatiara@gmail.com>
2025-07-14 09:56:49 +0700

    count dashboard

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -4,11 +4,45 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Jabatan;
+use App\Models\Bidang;
+use App\Models\Pegawai;
+use App\Models\Berita;
+use App\Models\pengumuman;
+use App\Models\Galeri;
+use App\Models\Video;
+use App\Models\Layanan;
+use App\Models\Dokumen;
+use App\Models\Kontak;
+use App\Models\Lokasi;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard');
+
+        $jumlahJabatan = Jabatan::count();
+        $jumlahBidang = Bidang::count();
+        $jumlahPegawai = Pegawai::count();
+        $jumlahBerita = Berita::count();
+        $jumlahPengumuman = Pengumuman::count();
+        $jumlahGaleri = Galeri::count();
+        $jumlahVideo = Video::count();
+        $jumlahLayanan = Layanan::count();
+        $jumlahDokumen = Dokumen::count();
+        $jumlahPeta = Lokasi::count();
+
+        return view('backend.dashboard', [
+        'jumlahJabatan' => $jumlahJabatan,
+        'jumlahBidang' => $jumlahBidang,
+        'jumlahPegawai' => $jumlahPegawai,
+        'jumlahBerita' => $jumlahBerita,
+        'jumlahPengumuman' => $jumlahPengumuman,
+        'jumlahGaleri' => $jumlahGaleri,
+        'jumlahVideo' => $jumlahVideo,
+        'jumlahLayanan' => $jumlahLayanan,
+        'jumlahDokumen' => $jumlahDokumen,
+        'jumlahPeta' => $jumlahPeta,
+        ]);
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -13,68 +13,174 @@
 
     <!-- Small boxes (Stat box) -->
     <div class="row">
+
       <div class="col-lg-3 col-6">
         <!-- small box -->
         <div class="small-box bg-info">
           <div class="inner">
-            <h3>150</h3>
+            <h3>{{$jumlahBidang}}</h3>
 
-            <p>New Orders</p>
+            <p>Data Bidang</p>
           </div>
           <div class="icon">
-            <i class="ion ion-bag"></i>
+            <i class="ion ion-stats-bars"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          {{-- <a href="{{ route('bidang.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
         </div>
       </div>
+
       <!-- ./col -->
       <div class="col-lg-3 col-6">
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+             <h3>{{$jumlahJabatan}}</h3>
 
-            <p>Bounce Rate</p>
+            <p>Data Jabatan</p>
           </div>
           <div class="icon">
             <i class="ion ion-stats-bars"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
         </div>
       </div>
+
       <!-- ./col -->
       <div class="col-lg-3 col-6">
         <!-- small box -->
         <div class="small-box bg-warning">
-          <div class="inner">
-            <h3>44</h3>
+           <div class="inner">
+             <h3>{{$jumlahPegawai}}</h3>
 
-            <p>User Registrations</p>
+            <p>Data Pegawai</p>
           </div>
           <div class="icon">
-            <i class="ion ion-person-add"></i>
+            <i class="ion ion-stats-bars"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
         </div>
       </div>
+
       <!-- ./col -->
       <div class="col-lg-3 col-6">
         <!-- small box -->
         <div class="small-box bg-danger">
-          <div class="inner">
-            <h3>65</h3>
+           <div class="inner">
+             <h3>{{$jumlahBerita}}</h3>
 
-            <p>Unique Visitors</p>
+            <p>Data Berita</p>
           </div>
           <div class="icon">
-            <i class="ion ion-pie-graph"></i>
+            <i class="ion ion-stats-bars"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
         </div>
       </div>
+      
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>{{$jumlahPengumuman}}</h3>
+
+            <p>Data Pengumuman</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          {{-- <a href="{{ route('bidang.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+        </div>
+      </div>
+
       <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+          <div class="inner">
+             <h3>{{$jumlahGaleri}}</h3>
+
+            <p>Data Galeri</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+        </div>
+      </div>
+
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+           <div class="inner">
+             <h3>{{$jumlahVideo}}</h3>
+
+            <p>Data Video</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+        </div>
+      </div>
+
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+           <div class="inner">
+             <h3>{{$jumlahLayanan}}</h3>
+
+            <p>Data Layanan</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+        </div>
+      </div>
+      
     </div>
     <!-- /.row -->
+
+    <div class="row">
+
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>{{$jumlahDokumen}}</h3>
+
+            <p>Data Dokumen</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          {{-- <a href="{{ route('bidang.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+        </div>
+      </div>
+
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+          <div class="inner">
+             <h3>{{$jumlahPeta}}</h3>
+
+            <p>Data Peta Jaringan</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+        </div>
+      </div>
+
+    </div>
 
     <!-- Main row -->
     <div class="row">
