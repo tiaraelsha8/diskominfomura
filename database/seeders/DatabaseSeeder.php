f2f909241e74c6c60c52e664fd9dd5f018c5fff9
ArLynx <anggaramadani123@gmail.com>
2025-07-14 08:18:30 +0700

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bidang;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserSeeder::class,]);
+        $this->call([UserSeeder::class,BidangSeeder::class,JabatanSeeder::class]);
     }
 }
