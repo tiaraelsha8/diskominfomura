7a75150a59a4417041a94c0a450d56817048532d
ArLynx <anggaramadani123@gmail.com>
2025-07-23 15:05:06 +0700

    edit download data pegawai

--- resources/views/frontend/pegawai/index.blade.php
+++ resources/views/frontend/pegawai/index.blade.php
@@ -17,10 +17,11 @@
     <script src="https://balkan.app/js/OrgChart.js"></script>
 
     <script>
+        OrgChart.templates.myTemplate = Object.assign({}, OrgChart.templates.ula);
         $(document).ready(function() {
             const data = @json($nodes);
             let chart = new OrgChart(document.getElementById("chart-org"), {
-                template: "ula",
+                template: "myTemplate",
                 mode: 'light',
                 enableAI: true,
                 layout: OrgChart.mixed,
@@ -31,17 +32,26 @@
                 nodeMenu: {
                     download: {
                         text: "Download File LHKPN",
-                        icon: OrgChart.icon.pdf(24, 24, "#039BE5"), // Optional: tambah ikon
+                        icon: OrgChart.icon.pdf(24, 24, "#039BE5"),
                         onClick: function(args) {
-                            console.log("Node diklik:", args.node); // Lihat semua properti yang tersedia
+
+                            const fileUrl = data[args].file_link;
+                            console.log("Node diklik:", data[args].file_link); 
+                            if (fileUrl) {
+                                window.open(fileUrl, '_blank');
+                            } else {
+                                alert("File LHKPN tidak tersedia.");
+                            }
                         }
-                    },
+                    }
                 },
+
                 nodeBinding: {
                     field_0: "name", // Nama pegawai
                     field_1: "title", // Jabatan
                     field_2: "desc", // Tupoksi
-                    field_3: "bidang", // Nama bidang
+                    field_3: "bidang",
+                    field_4: "file_link", // Nama bidang
                     img_0: "img" // Foto
                 }
             });
