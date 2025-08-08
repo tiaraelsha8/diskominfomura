2fbb165d04aeffb0464a5a9d98e0fbd0de54404a
unknown <elshawinatiara@gmail.com>
2025-08-08 14:51:25 +0700

    data statistik subdomain

--- app/Helpers/VisitorCounter.php
+++ app/Helpers/VisitorCounter.php
@@ -8,25 +8,34 @@ class VisitorCounter
 {
     public static function count()
     {
+        $host = request()->getHost();
+        if ($host !== 'diskominfo.murungraya.go.id') {
+            return [
+                'total' => 0,
+                'today' => 0,
+                'online' => 0,
+            ];
+        }
+
         $ip = request()->ip();
         $now = time();
         $timeout = 300; // 5 menit
 
         $date = date('Y-m-d');
-        $totalFile = "counter/total.txt";
+        $totalFile = 'counter/total.txt';
         $todayFile = "counter/today-$date.txt";
-        $onlineFile = "counter/online.json";
+        $onlineFile = 'counter/online.json';
 
         // === Sesi Laravel
         if (!session()->has('has_visited')) {
             session(['has_visited' => true]);
 
             // Total
-            $total = (int)Storage::get($totalFile) ?? 0;
+            $total = (int) Storage::get($totalFile) ?? 0;
             Storage::put($totalFile, $total + 1);
 
             // Hari ini
-            $today = (int)Storage::get($todayFile) ?? 0;
+            $today = (int) Storage::get($todayFile) ?? 0;
             Storage::put($todayFile, $today + 1);
         }
 
@@ -43,9 +52,9 @@ class VisitorCounter
         Storage::put($onlineFile, json_encode($online));
 
         return [
-            'total' => (int)Storage::get($totalFile),
-            'today' => (int)Storage::get($todayFile),
-            'online' => count($online)
+            'total' => (int) Storage::get($totalFile),
+            'today' => (int) Storage::get($todayFile),
+            'online' => count($online),
         ];
     }
 }
