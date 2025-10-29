9ba237158f1a3ebcf3611342eaf7c97bb787d38d
ArLynx <anggaramadani123@gmail.com>
2025-10-29 09:40:12 +0700

    perbaikan

--- resources/views/frontend/kontak/index.blade.php
+++ resources/views/frontend/kontak/index.blade.php
@@ -268,7 +268,7 @@
         <div class="hero-text">
             <h1>Hubungi Kami</h1>
             <p>
-                Punya pertanyaan? Anda mungkin dapat menemukan jawabannya di <a href="#">Kontak Person Disdukcapil</a>.
+                Punya pertanyaan? Anda mungkin dapat menemukan jawabannya di <a href="#">Kontak Person</a>.
                 Berikut adalah berbagai cara untuk menghubungi tim kami.
             </p>
         </div>
