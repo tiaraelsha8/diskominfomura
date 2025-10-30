de4b3d46d5e99f6b5e5fbac717b9c27e57971310
ArLynx <anggaramadani123@gmail.com>
2025-10-30 09:31:39 +0700

    perbaikan bidang

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -259,7 +259,7 @@
             line-height: 1.8;
             color: #333;
             margin-bottom: 0.3em;
-            text-align: justify;
+            /* text-align: justify; */
         }
 
         .galeri-home-section {
