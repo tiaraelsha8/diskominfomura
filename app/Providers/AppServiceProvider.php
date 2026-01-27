86d88ae29663a5bd5b65fbd9b5acb06ad00de77c
unknown <anggaramadani123@gmail.com>
2026-01-27 10:47:35 +0700

    edit ukuran berita

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator as PaginationPaginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         PaginationPaginator::useBootstrapFive();
+        //URL::forceScheme('https');
     }
 }
