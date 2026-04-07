3ab7b164be5611c87632a2769daa72442897baba
unknown <anggaramadani123@gmail.com>
2026-04-07 10:58:48 +0700

    edit home pengumuman

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -979,7 +979,7 @@
                 </div>
                 <div class="galeri-card-body">
                     <h3>Pengumuman</h3>
-                    <p>Pengumuman resmi dan pemberitahuan dari Diskominfo.</p>
+                    <p>Pengumuman resmi dan pemberitahuan.</p>
                 </div>
             </a>
 
@@ -1003,7 +1003,7 @@
                 @foreach ($carousel as $item)
                     "{{ asset('storage/carousel/' . $item->foto) }}",
                 @endforeach
-                ];
+                                ];
 
             let index = 0;
             const imgElement = document.getElementById('carouselImage');
