824d8f87761f43cfd43aac90a85d54d3b6f959d9
unknown <anggaramadani123@gmail.com>
2026-04-07 10:57:41 +0700

    edit home pengumuman

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -905,9 +905,9 @@
         @endif
 
         <!-- <div class="hero-container" data-aos="fade-down">
-                                                <h1>Selamat Datang <br> Di Kecamatan Murung<br> Kabupaten Murung Raya</h1>
-                                                <p data-aos="fade-up" data-aos-delay="200">Murung Raya Hebat</p>
-                                            </div> -->
+                                                                    <h1>Selamat Datang <br> Di Kecamatan Murung<br> Kabupaten Murung Raya</h1>
+                                                                    <p data-aos="fade-up" data-aos-delay="200">Murung Raya Hebat</p>
+                                                                </div> -->
     </div>
 
     <section class="album-home-section">
@@ -1066,7 +1066,7 @@
                 </div>
                 <div class="galeri-card-body">
                     <h3>Pengumuman</h3>
-                    <p>Pengumuman resmi dan pemberitahuan dari Diskominfo.</p>
+                    <p>Pengumuman resmi dan pemberitahuan.</p>
                 </div>
             </a>
 
@@ -1141,7 +1141,7 @@
                 @foreach ($carousel as $item)
                     "{{ asset('storage/carousel/' . $item->foto) }}",
                 @endforeach
-                                                                                                                                                                                                                                                                                                                                    ];
+                                                                                                                                                                                                                                                                                                                                                        ];
 
             let index = 0;
             const imgElement = document.getElementById('carouselImage');
